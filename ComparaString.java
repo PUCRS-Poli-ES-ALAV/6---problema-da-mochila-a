5bd8bcd6a33c6c6360ec53e619b6155b1e1af732
AugustoKnob <augusto.knob@edu.pucrs.br>
2025-09-17 23:01:08 +0000

    Distancia edição terminado

--- ComparaString.java
+++ ComparaString.java
@@ -1,12 +1,15 @@
 public class ComparaString {
 
+    public static int distanciaEdicao(String s1, String s2){
+        return distanciaEdicaoRec(s1, s2,s1.length()-1, s2.length()-1);
+    }
 
-    public int distanciaEdicao(String s1, String s2, int i, int j){
-        if(s1.isBlank()) return s2.length();
-        if(s2.isBlank()) return s1.length();
+    private static int distanciaEdicaoRec(String s1, String s2, int i, int j){
+        if(i == 0) return j;
+        if(j==0) return i;
         if(s1.charAt(i) == s2.charAt(j)){
-            return 0 + distanciaEdicao(s1, s2, i-1, j-1);
+            return 0 + distanciaEdicaoRec(s1, s2, i-1, j-1);
         }
-        return Math.min();
+        return 1 + Math.min(Math.min(distanciaEdicaoRec(s1,s2,i-1,j-1),distanciaEdicaoRec(s1,s2,i,j-1)),distanciaEdicaoRec(s1,s2,i-1,j));
     }
 }
