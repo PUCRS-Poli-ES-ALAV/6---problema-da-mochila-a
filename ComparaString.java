27019cd79a0387debbd7d34fb3f24d0588f67e64
AugustoKnob <augusto.knob@edu.pucrs.br>
2025-09-17 23:08:43 +0000

    Conta iterações

--- ComparaString.java
+++ ComparaString.java
@@ -1,15 +1,27 @@
 public class ComparaString {
 
+    private static int iteracoes = 0;
+
     public static int distanciaEdicaoForcaBruta(String s1, String s2){
-        return distanciaEdicaoForcaBrutaRec(s1, s2,s1.length()-1, s2.length()-1);
+        iteracoes = 0;
+        int resultado = distanciaEdicaoForcaBrutaRec(s1, s2, s1.length()-1, s2.length()-1);
+        System.out.println("Iterações: " + iteracoes);
+        return resultado;
     }
 
     private static int distanciaEdicaoForcaBrutaRec(String s1, String s2, int i, int j){
+        iteracoes++;
         if(i == 0) return j;
-        if(j==0) return i;
+        if(j == 0) return i;
         if(s1.charAt(i) == s2.charAt(j)){
-            return 0 + distanciaEdicaoForcaBrutaRec(s1, s2, i-1, j-1);
+            return distanciaEdicaoForcaBrutaRec(s1, s2, i-1, j-1);
         }
-        return 1 + Math.min(Math.min(distanciaEdicaoForcaBrutaRec(s1,s2,i-1,j-1),distanciaEdicaoForcaBrutaRec(s1,s2,i,j-1)),distanciaEdicaoForcaBrutaRec(s1,s2,i-1,j));
+        return 1 + Math.min(
+            Math.min(
+                distanciaEdicaoForcaBrutaRec(s1, s2, i-1, j-1),
+                distanciaEdicaoForcaBrutaRec(s1, s2, i, j-1)
+            ),
+            distanciaEdicaoForcaBrutaRec(s1, s2, i-1, j)
+        );
     }
 }
