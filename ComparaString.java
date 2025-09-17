a28aca67426cd33cd0a67c7ae5cec7dc2edbcadc
AugustoKnob <augusto.knob@edu.pucrs.br>
2025-09-17 23:27:31 +0000

    Programação dinamica

--- ComparaString.java
+++ ComparaString.java
@@ -2,6 +2,43 @@ public class ComparaString {
 
     private static int iteracoes = 0;
 
+    public static int distanciaEdicaoPD(String s1, String s2){
+        int m = s1.length();
+        int n = s2.length();
+        int[][] matriz = new int[m][n];
+        matriz[0][0] = 0;
+
+        for (int i = 0; i < m; i++){
+            matriz[i][0] = (i == 0) ? 0 : matriz[i-1][0] + 1;
+        }
+
+        for (int j = 0; j < n; j++){
+            matriz[0][j] = (j == 0) ? 0 : matriz[0][j-1] + 1;
+        }
+
+        int custoExtra = 0;
+        int iteracoesPD = 0;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                iteracoesPD++;
+                if(s1.charAt(i) == s2.charAt(j)){
+                    custoExtra = 0;
+                }
+                else{
+                    custoExtra = 1;
+                }
+                matriz[i][j] = Math.min(
+                    Math.min(matriz[i-1][j] + 1, matriz[i][j-1] + 1),
+                    matriz[i-1][j-1] + custoExtra
+                );
+            }
+        }
+
+        System.out.println("Iterações PD: " + iteracoesPD);
+        return matriz[m-1][n-1];
+    }
+
+
     public static int distanciaEdicaoForcaBruta(String s1, String s2){
         iteracoes = 0;
         int resultado = distanciaEdicaoForcaBrutaRec(s1, s2, s1.length()-1, s2.length()-1);
